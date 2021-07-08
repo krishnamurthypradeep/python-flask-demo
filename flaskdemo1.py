6c0b0401d26cc3f7acb6d144fac9e944e617d12e
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2021-07-09 00:16:29 +0530

    first python project commit dkdjk

--- flaskdemo1.py
+++ flaskdemo1.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 # http://localhost:8080/
 @app.route("/home")
 def welcome():
-    return "jekjdkjdNew Relay Forward it works"
+    return "jekjdkjdNkdlkdlkew Relay Forward it works"
